8043568c2eee36be731497ea3f25f271a36d99a9
JinXiaozhao <Jin_xiaozhao@163.com>
2020-07-21 23:10:13 +0800

    完善bug

--- my_blog/www/orm.py
+++ my_blog/www/orm.py
@@ -12,14 +12,14 @@ def log(sql,args=()):
 #建立数据库连接池
 async def create_pool(loop,**kw):
     global __pool
-    logging,info("create mysql database connection pool....")
+    logging.info("create mysql database connection pool....")
     __pool = await aiomysql.create_pool(
-        host=kw.get("host",'127.0.0.1'),
+        host=kw.get("host",'localhost'),
         port=kw.get("port",3306),
         user=kw.get("user",'root'),
         password=kw.get("password",'109801'),
         db=kw.get("db",'blog'),
-        charset=kw.get("charset","utf-8")
+        charset=kw.get("charset","utf8"),
         autocommit=kw.get("autocommit",True),
         maxsize=kw.get("maxsize",10),
         minsize=kw.get("minsize",1),
@@ -69,46 +69,46 @@ def create_args_string(num):
   
 
 #描述字段的字段名，数据类型，键信息，默认值
-class Filed():
+class Field():
 
     def __init__(self,name,column_type,primary_key,default):
         self.name = name
         self.column_type = column_type
-        self.primary_key = prinmary_key
+        self.primary_key = primary_key
         self.default = default
 
     def __str__(self):
-        return '<%s,%s:%s>' % (self.__class__.name,self.column_type,self.name)
+        return '<%s,%s:%s>' % (self.__class__.__name__,self.column_type,self.name)
 
 
-class StringFiled(Filed):
+class StringField(Field):
 
     def __init__(self,name=None,column_type='varchar(100)',primary_key=False,default=None):
-        super.__init__(name,column_type,primary_key,default)
+        super().__init__(name,column_type,primary_key,default)
 
 
-class BooleanFiled(Filed):
+class BooleanField(Field):
 
     def __init__(self,name=None,default=False):
-        super.__init__(name,'boolean',False,default)
+        super().__init__(name,'boolean',False,default)
 
 
-class IntegerFiled(Filed):
+class IntegerField(Field):
     
     def __init__(self,name=None,primary_key=False,default=0):
-        super.__init__(name,'bigint',primary_key,default)
+        super().__init__(name,'bigint',primary_key,default)
 
 
-class FloatFiled(Filed):
+class FloatField(Field):
     
     def __init__(self,name=None,primary_key=False,default=0.0):
-        super.__init__(name,'real',primary_key,default)
+        super().__init__(name,'real',primary_key,default)
 
 
-class TextFiled(Filed):
+class TextField(Field):
     
     def __init__(self,name=None,default=None):
-        super.__init__(name,'text',False,default)
+        super().__init__(name,'text',False,default)
 
 
 # 元类用来动态的创建类
@@ -144,16 +144,17 @@ class ModelMetaclass(type):
         escaped_fields = list(map(lambda f: '`%s`' % f, fields))
         attrs['__mappings__'] = mappings
         attrs['__table__'] = table_name
+        attrs['__primary_key__'] = primaryKey
         attrs['__fields__'] = fields
         
-        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
-        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
-        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey) 
-        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), table_name)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (table_name, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (table_name, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey) 
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (table_name, primaryKey)
         return type.__new__(cls,name,bases,attrs)
 
 
-class Model(dict,metaclass=ModelMateclass):
+class Model(dict,metaclass=ModelMetaclass):
 
     def __init__(self,**kw):
         super(Model,self).__init__(**kw)
